0b2d742bee9d21f7db44101db4c02135fff4a6dd
Daniyar Kurmangaliiev <randomna79@gmail.com>
2020-08-31 02:59:19 +0600

    Исправлена ошибка flags of undefined

--- events/discord/RoleMemberAdd.js
+++ events/discord/RoleMemberAdd.js
@@ -12,37 +12,39 @@ const execute = async (self, member, role) => {
 
     if (member.guild.id == '740586549145763960' && ['746826292900528311', '746825558205136926', '746752483115794583'].includes(role.id)) {
         const user = await self.db.users.fetch({ _id: member.id })
-
-        if (role.id == '746826292900528311') {
-            await self.db.users.update({ _id: member.id }, {
-                $set: {
-                    flags: user.flags | 1 << 0,
-                    'boost.available': true,
-                    'boost.type': 'DEVELOPER',
-                    'boost.tier': 100
-                }
-            })
-        }
-
-        if (role.id == '746825558205136926') {
-            await self.db.users.update({ _id: member.id }, {
-                $set: {
-                    flags: user.flags | 1 << 1,
-                    'boost.available': true,
-                    'boost.type': 'TEAM',
-                    'boost.tier': user.boost.tier || 2
-                }
-            })
-        }
-
-        if (role.id == '746752483115794583' && !user.boost.available) {
-            await self.db.users.update({ _id: member.id }, {
-                $set: {
-                    'boost.available': true,
-                    'boost.type': 'SERVER_BOOST',
-                    'boost.tier': 1
-                }
-            })
+        
+        if (user) {
+            if (role.id == '746826292900528311') {
+                await self.db.users.update({ _id: member.id }, {
+                    $set: {
+                        flags: user.flags | 1 << 0,
+                        'boost.available': true,
+                        'boost.type': 'DEVELOPER',
+                        'boost.tier': 100
+                    }
+                })
+            }
+    
+            if (role.id == '746825558205136926') {
+                await self.db.users.update({ _id: member.id }, {
+                    $set: {
+                        flags: user.flags | 1 << 1,
+                        'boost.available': true,
+                        'boost.type': 'TEAM',
+                        'boost.tier': user.boost.tier || 2
+                    }
+                })
+            }
+    
+            if (role.id == '746752483115794583' && !user.boost.available) {
+                await self.db.users.update({ _id: member.id }, {
+                    $set: {
+                        'boost.available': true,
+                        'boost.type': 'SERVER_BOOST',
+                        'boost.tier': 1
+                    }
+                })
+            }
         }
     }
 
