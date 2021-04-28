f105afa44352f757d0247f829b6189bea07aeeac
Daniyar Kurmangaliiev <randomna79@gmail.com>
2021-04-28 21:57:50 +0600

    Access owners to all dashboard

--- internals/api/utility/Authorize.js
+++ internals/api/utility/Authorize.js
@@ -56,15 +56,15 @@ module.exports.permitted = async function (req, res, next) {
     }
 
     const guild = guilds.find(g => g.id == guild_id)
+    const owner = await ShardingManager.shards.first().eval(`this.application.owner.members.some(m => m.id == ${user_id})`)
 
-    if (!guild) {
+    if (!guild && !owner) {
         await res.status(404).send('Not Found')
 
         return
     }
 
     const permissions = new Permissions(guild.permissions)
-    const owner = await ShardingManager.shards.first().eval(`this.application.owner.members.some(m => m.id == ${user_id})`)
 
     if (!guild.owner && !permissions.has('ADMINISTRATOR') && !owner) {
         await res.status(403).send('Forbidden')
