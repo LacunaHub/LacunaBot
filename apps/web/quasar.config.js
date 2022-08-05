29492fe619105b233434a9001be36dba6a19aa6f
Daniyar Kurmangaliyev <randomna79@gmail.com>
2022-08-05 23:34:46 +0600

    analytics.js missing in boot

--- apps/web/quasar.config.js
+++ apps/web/quasar.config.js
@@ -28,7 +28,7 @@ module.exports = configure(function (ctx) {
         // app boot file (/src/boot)
         // --> boot files are part of "main.js"
         // https://v2.quasar.dev/quasar-cli/boot-files
-        boot: ['i18n', 'axios', 'emoji-picker', 'luxon', 'markdown', 'numbro'],
+        boot: ['analytics', 'i18n', 'axios', 'emoji-picker', 'luxon', 'markdown', 'numbro'],
 
         // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
         css: ['app.scss', 'style.scss', 'emoji-picker.scss'],
