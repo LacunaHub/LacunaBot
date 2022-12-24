ad3f3886bc3eada09a917a19e4f931d513a2794e
Daniyar Kurmangaliyev <randomna79@gmail.com>
2022-12-25 00:58:55 +0600

    Cluster extends

--- src/internals/Cluster.ts
+++ src/internals/Cluster.ts
@@ -1,5 +1,5 @@
 import { Client as BridgeClient } from 'discord-cross-hosting'
-import { Manager as ClusterManager } from 'discord-hybrid-sharding'
+import { HeartbeatManager, Manager as ClusterManager } from 'discord-hybrid-sharding'
 import logger from './Logger'
 
 const bridgeClient = new BridgeClient({
@@ -15,8 +15,19 @@ bridgeClient.on('ready', () => logger.info('[BridgeClient] Client is ready'))
 
 const clusterManager = new ClusterManager(`${__dirname}/Client.js`, {
     token: process.env.DISCORD_CLIENT_TOKEN,
-    shardsPerClusters: Number(process.env.DISCORD_CLIENT_SHARDS_PER_CLUSTER)
+    shardsPerClusters: Number(process.env.DISCORD_CLIENT_SHARDS_PER_CLUSTER),
+    restarts: {
+        max: 5,
+        interval: 3_600_000
+    }
 })
+
+clusterManager.extend(
+    new HeartbeatManager({
+        interval: 2000,
+        maxMissedHeartbeats: 10
+    })
+)
 
 clusterManager.on('clusterCreate', cluster => logger.info(`[ClusterManager] Cluster #${cluster.id} has been created`))
 
