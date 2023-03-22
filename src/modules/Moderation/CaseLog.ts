8f3b9fff5128812d8e9499d9d2d2d2adf3d13e83
Daniyar Kurmangaliyev <randomna79@gmail.com>
2023-03-22 10:49:38 +0600

    Изменение причины случая в модальном окне

--- src/modules/Moderation/CaseLog.ts
+++ src/modules/Moderation/CaseLog.ts
@@ -1,7 +1,22 @@
-import { BaseGuildTextChannel, EmbedBuilder, Guild, User } from 'discord.js'
-import reason from '../../commands/moderation/reason'
+import {
+    ActionRowBuilder,
+    BaseGuildTextChannel,
+    ButtonBuilder,
+    ButtonInteraction,
+    ButtonStyle,
+    EmbedBuilder,
+    Guild,
+    ModalActionRowComponentBuilder,
+    ModalBuilder,
+    ModalSubmitInteraction,
+    TextInputBuilder,
+    TextInputStyle,
+    User
+} from 'discord.js'
 import db from '../../database'
+import { ServerDocument } from '../../database/schemas/Servers'
 import i18n from '../../i18n'
+import Lacuna from '../../internals/Lacuna'
 import { images } from '../Logs'
 
 export async function createCaseEntry(guild: Guild, options: ICreateCaseMessageOptions) {
@@ -12,8 +27,8 @@ export async function createCaseEntry(guild: Guild, options: ICreateCaseMessageO
         const t = i18n.t.bind(null, server.locale)
         const caseId = server.moderation.case_log.case_count + 1
 
-        await caseLog
-            .send({
+        try {
+            await caseLog.send({
                 embeds: [
                     new EmbedBuilder()
                         .setAuthor({ name: t(`case_log.cases.${options.type}`), iconURL: images[options.type] })
@@ -29,9 +44,16 @@ export async function createCaseEntry(guild: Guild, options: ICreateCaseMessageO
                         .setFooter({ text: t('case_log.case_number', { case_number: caseId }) })
                         .setTimestamp()
                         .setColor(options.type.endsWith('REMOVE') ? '#2FDF84' : '#EF5350')
+                ],
+                components: [
+                    new ActionRowBuilder<ButtonBuilder>().addComponents(
+                        new ButtonBuilder().setCustomId(`CL-REASON-${caseId}`).setLabel(t('case_log.change_reason')).setStyle(ButtonStyle.Secondary)
+                    )
                 ]
             })
-            .catch(() => {})
+        } catch (err) {
+            return
+        }
 
         await db.servers.updateOne(
             { _id: server._id },
@@ -44,7 +66,7 @@ export async function createCaseEntry(guild: Guild, options: ICreateCaseMessageO
                         case_id: caseId,
                         type: options.type,
                         timestamp: Date.now(),
-                        reason: reason ?? null,
+                        reason: options.reason ?? null,
                         target: {
                             id: options.target ? options.target.id : null,
                             name: options.target ? options.target.tag : null
@@ -66,6 +88,94 @@ export async function createCaseEntry(guild: Guild, options: ICreateCaseMessageO
             target: { id: options.target.id, name: options.target.tag }
         })
     }
+}
+
+export async function onPressChangeReasonButton(self: Lacuna, server: ServerDocument, interaction: ButtonInteraction) {
+    if (!interaction.memberPermissions.has(self.PermissionFlags.ManageMessages)) {
+        await interaction.reply({
+            content: `${self._emojis.ERROR} | ${self.i18n.t(server.locale, 'common.command_denied', {
+                user: `**${interaction.user.username}**`
+            })}`,
+            ephemeral: true
+        })
+
+        return false
+    }
+
+    const [, , caseId] = interaction.customId.split('-')
+    const caseEntry = server.moderation.case_log.cases.find(i => i.case_id === Number(caseId))
+
+    if (!caseEntry) {
+        await interaction.reply({
+            content: `${self._emojis.ERROR} | ${self.i18n.t(server.locale, 'commands.reason.text_no_case_entry', {
+                user: `**${interaction.user.username}**`
+            })}`,
+            ephemeral: true
+        })
+
+        return false
+    }
+
+    const modal = new ModalBuilder()
+        .setCustomId(`CL-REASON-${caseId}`)
+        .setTitle(self.i18n.t(server.locale, 'case_log.case_number', { case_number: caseId }))
+        .addComponents(
+            new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+                new TextInputBuilder()
+                    .setCustomId('CL-REASON')
+                    .setLabel(self.i18n.t(server.locale, 'case_log.reason'))
+                    .setStyle(TextInputStyle.Paragraph)
+                    .setMaxLength(1000)
+                    .setRequired(true)
+                    .setValue(typeof caseEntry.reason === 'string' ? caseEntry.reason : '')
+            )
+        )
+
+    await interaction.showModal(modal)
+}
+
+export async function onSubmitChangeReasonModal(self: Lacuna, server: ServerDocument, interaction: ModalSubmitInteraction) {
+    const reason = interaction.fields.getTextInputValue('CL-REASON').trim()
+
+    if (!reason) {
+        await interaction.reply({
+            content: `${self._emojis.ERROR} | ${self.i18n.t(server.locale, 'commands.reason.text_no_reason', {
+                user: `**${interaction.user.username}**`
+            })}`,
+            ephemeral: true
+        })
+
+        return false
+    }
+
+    let [, , caseId] = interaction.customId.split('-')
+    const embed = new EmbedBuilder(interaction.message.embeds[0].toJSON())
+    embed.data.fields[2].value = reason
+
+    await interaction.message.edit({ embeds: [embed] })
+    await self.db.servers.updateOne(
+        { _id: interaction.guild.id, 'moderation.case_log.cases.case_id': Number(caseId) },
+        {
+            $set: {
+                'moderation.case_log.cases.$.reason': reason
+            }
+        }
+    )
+
+    await interaction.reply({
+        content: `${self._emojis.OK} | ${self.i18n.t(server.locale, 'commands.reason.text_case_edited', {
+            user: `**${interaction.user.username}**`
+        })}`,
+        ephemeral: true
+    })
+
+    self.emit('moduleExecution', {
+        module: 'Moderation',
+        category: 'CaseLog',
+        label: 'ChangeReason',
+        guild: { id: interaction.guildId, name: interaction.guild.name },
+        target: { id: interaction.user.id, name: interaction.user.tag }
+    })
 }
 
 export interface ICreateCaseMessageOptions {
