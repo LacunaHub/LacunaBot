8913624252766c6be84d27540b975698d82d237c
Daniyar Kurmangaliyev <randomna79@gmail.com>
2023-04-03 04:05:00 +0600

    Added banner to user command

--- src/commands/general/user/slash.ts
+++ src/commands/general/user/slash.ts
@@ -13,11 +13,13 @@ export default async (self: Lacuna, server: ServerDocument, interaction: ChatInp
     const created_ts = Math.round(mention.user.createdTimestamp / 1000)
     const joined_ts = Math.round(mention.joinedTimestamp / 1000)
 
+    await mention.user.fetch()
+
     const embed = new EmbedBuilder()
         .setAuthor({ name, iconURL: mention.user.displayAvatarURL() })
         .addFields([
-            { name: t('commands.user.text_registration_date'), value: `<t:${created_ts}:d> – <t:${created_ts}:R>`, inline: true },
-            { name: t('commands.user.text_join_date'), value: `<t:${joined_ts}:d> – <t:${joined_ts}:R>`, inline: true },
+            { name: t('commands.user.text_registration_date'), value: `<t:${created_ts}:R>`, inline: true },
+            { name: t('commands.user.text_join_date'), value: `<t:${joined_ts}:R>`, inline: true },
             {
                 name: `${t('common.roles')} [${mention.roles.cache.filter(r => r.id != interaction.guild.id).size}]`,
                 value:
@@ -56,8 +58,18 @@ export default async (self: Lacuna, server: ServerDocument, interaction: ChatInp
         new ButtonBuilder()
             .setStyle(ButtonStyle.Link)
             .setLabel(t('commands.user.text_avatar_link'))
-            .setURL(mention.user.displayAvatarURL({ size: 512, extension: 'png' }))
+            .setURL(mention.user.displayAvatarURL({ size: 512 }))
     )
+
+    if (mention.user.banner) {
+        embed.setImage(mention.user.bannerURL({ size: 512 }))
+        row.addComponents(
+            new ButtonBuilder()
+                .setStyle(ButtonStyle.Link)
+                .setLabel(t('commands.user.text_banner_link'))
+                .setURL(mention.user.bannerURL({ size: 1024 }))
+        )
+    }
 
     await interaction.reply({ embeds: [embed], components: [row] })
 
