e20ddb04c026b821fdef54b06e99de4225fc20a9
Daniyar Kurmangaliyev <randomna79@gmail.com>
2023-09-11 00:28:24 +0600

    fix(server logs): check permission for download endpoint

--- src/internals/api/routes/guilds.ts
+++ src/internals/api/routes/guilds.ts
@@ -35,7 +35,7 @@ router.post('/:guild_id/subscriptions/telegram/:method', createRateLimitMiddlewa
 router.post('/:guild_id/subscriptions/twitch/:method', createRateLimitMiddleware(5), authorize, checkPermissions, updateTwitchSubscriptions)
 router.post('/:guild_id/subscriptions/youtube/:method', createRateLimitMiddleware(5), authorize, checkPermissions, updateYouTubeSubscriptions)
 router.post('/:guild_id/transfer-diamond/:to_guild_id', createRateLimitMiddleware(1, 1000 * 60 * 5), authorize, transferDiamond)
-router.post('/:guild_id/download-logs', createRateLimitMiddleware(5), authorize, downloadLogs)
+router.post('/:guild_id/download-logs', createRateLimitMiddleware(5), authorize, checkPermissions, downloadLogs)
 
 async function getSettings(ctx: Context) {
     const guild_id: string = ctx.params.guild_id
