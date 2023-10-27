e14c6d7e4e370e344a942c1d43a3b7b267986fe3
Daniyar Kurmangaliyev <randomna79@gmail.com>
2023-10-27 07:16:36 +0600

    style(Client): rejectOnRateLimit

--- src/internals/Client.ts
+++ src/internals/Client.ts
@@ -52,9 +52,7 @@ const client = new Lacuna({
         return new Collection()
     },
     rest: {
-        rejectOnRateLimit: rateLimitData => {
-            return rateLimitData.global || rateLimitData.timeToReset >= 1000 * 60 * 5
-        }
+        rejectOnRateLimit: rateLimitData => rateLimitData.global || rateLimitData.timeToReset >= 1000 * 60 * 5
     },
     sweepers: {
         ...Options.DefaultSweeperSettings,
