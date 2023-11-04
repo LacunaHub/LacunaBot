0a06eb522257b72125bbb524a83406858a598952
Daniyar Kurmangaliyev <randomna79@gmail.com>
2023-11-05 00:41:51 +0600

    fix: discord rate limit handling

--- src/internals/Client.ts
+++ src/internals/Client.ts
@@ -52,7 +52,7 @@ const client = new Lacuna({
         return new Collection()
     },
     rest: {
-        rejectOnRateLimit: rateLimitData => true // rateLimitData.global || rateLimitData.timeToReset >= 1000 * 60 * 5
+        rejectOnRateLimit: rateLimitData => rateLimitData.global || rateLimitData.timeToReset >= 1000 * 60 * 5
     },
     sweepers: {
         ...Options.DefaultSweeperSettings,
