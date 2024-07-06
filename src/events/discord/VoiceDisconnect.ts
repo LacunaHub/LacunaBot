6186ce07455a3d55ba07568c386defd4ca20e7ae
Daniyar Kurmangaliyev <randomna79@gmail.com>
2024-07-06 17:01:01 +0500

    fix(Automation): `VOICE_DISCONNECT` event did not work
    feat(Automation): handle events in a single instance if there are more than 1 of them

--- src/events/discord/VoiceDisconnect.ts
+++ src/events/discord/VoiceDisconnect.ts
@@ -32,7 +32,7 @@ const handler = async (self: Lacuna, state: VoiceState, channel: VoiceChannel) =
     }
 
     await fetchGuild(self.cache, state.guild)
-    await Automation.handleEvent('VOICE_DISCONNECT', self, server, state)
+    await Automation.handleEvent('VOICE_DISCONNECT', self, server, state, { channelId: channel.id })
     await deleteTemporaryVoice(self, server, state, channel)
     await Levels.onVoiceDisconnect(self, server, state, channel)
     await economyVoiceUnassign(self, server, state, channel)
