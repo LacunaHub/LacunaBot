fe9dae4909739671d3815730a8a1450e5b758416
Daniyar Kurmangaliyev <30476012+daniquoll@users.noreply.github.com>
2024-08-26 01:31:20 +0500

    feat: autocomplete for duration argument (#281)

--- src/commands/moderation/mute/index.ts
+++ src/commands/moderation/mute/index.ts
@@ -16,7 +16,8 @@ const options: CommandOptions = {
             type: ApplicationCommandOptionType.String,
             name: 'Commands.Options.Duration',
             description: 'Commands.MuteCommand.Options.Duration.Description',
-            required: false
+            required: false,
+            autocomplete: true
         },
         {
             type: ApplicationCommandOptionType.String,
