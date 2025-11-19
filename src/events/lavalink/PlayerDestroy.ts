1ae297609be51c770beb56d1f3ae3f43234ff449
rovius <121901513+rovius@users.noreply.github.com>
2025-11-19 23:04:28 +0300

    feat: voice status (#357)
    * chore(deps): bump database driver to 1.1.8
    
    * feat(music): voice channel status
    
    * fix(TrackStart): incorrect action added to server log
    
    * fix(VoiceStatus): check for permissions first
    
    * feat(api): voice status settings API
    
    * fix(VoiceStatus): do not update status twice on player stop
    
    * chore(deps): bump lacuna-locale to 1.1.4
    
    * chore: bump version to 4.14.1

--- src/events/lavalink/PlayerDestroy.ts
+++ src/events/lavalink/PlayerDestroy.ts
@@ -1,3 +1,4 @@
+import { ServerDocument } from '@lacunahub/lacuna-database-driver'
 import { Player } from '@lacunahub/lavaluna.js'
 import { Message } from 'discord.js'
 import Lacuna from '../../internals/Lacuna'
@@ -5,6 +6,7 @@ import Lacuna from '../../internals/Lacuna'
 const handler = async (self: Lacuna, player: Player) => {
     const message = player.get<Message>('message'),
         timeout = player.get<NodeJS.Timeout>('timeout')
+    const server: ServerDocument = await self.db.servers.fetch({ _id: player.options.guildId })
 
     if (message) {
         try {
@@ -21,6 +23,18 @@ const handler = async (self: Lacuna, player: Player) => {
 
     if (timeout) {
         clearTimeout(timeout)
+    }
+
+    /**
+     * Remove voice channel status if enabled in settings.
+     * TODO: Use a proper method when discord.js has one.
+     * TODO: Use a proper permission instead of a BigInt when discord.js has one.
+     */
+    const selfHasStatusPermission = message.member.permissions.has(BigInt(281474976710656))
+    if (server.modules.music.voice_status.enabled && selfHasStatusPermission) {
+        self.rest.put(`/channels/${player.options.voiceChannelId}/voice-status`, {
+            body: { status: '' }
+        })
     }
 
     player.set('message', null)
--- src/events/lavalink/QueueEnd.ts
+++ src/events/lavalink/QueueEnd.ts
@@ -1,9 +1,12 @@
+import { ServerDocument } from '@lacunahub/lacuna-database-driver'
 import { Player } from '@lacunahub/lavaluna.js'
 import { Message } from 'discord.js'
 import Lacuna from '../../internals/Lacuna'
 
 const handler = async (self: Lacuna, player: Player) => {
     const message = player.get<Message>('message')
+    const server: ServerDocument = await self.db.servers.fetch({ _id: player.options.guildId })
+    const t = self.i18n.t.bind(null, server.locale)
 
     if (message) {
         try {
@@ -16,6 +19,18 @@ const handler = async (self: Lacuna, player: Player) => {
                 guild_id: player.guildId
             })
         }
+    }
+
+    /**
+     * Remove voice channel status if enabled in settings.
+     * TODO: Use a proper method when discord.js has one.
+     * TODO: Use a proper permission instead of a BigInt when discord.js has one.
+     */
+    const selfHasStatusPermission = message.member.permissions.has(BigInt(281474976710656))
+    if (server.modules.music.voice_status.enabled && player.state === 'CONNECTED' && selfHasStatusPermission) {
+        self.rest.put(`/channels/${player.options.voiceChannelId}/voice-status`, {
+            body: { status: t('Commands.QueueCommand.Texts.EmptyQueueStatus') }
+        })
     }
 
     player.set('message', null)
